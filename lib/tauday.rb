9a32b7d20a6d1709ba9fc2bae7e44005e22a34d2
Matthew Kerwin <matthew@kerwin.net.au>
2013-02-28 09:48:08 +1000

    more rdoc changes

--- lib/tauday.rb
+++ lib/tauday.rb
@@ -5,7 +5,21 @@ module Math
 end
 
 module BigMath
+  ##
   # Computes the value of tau to the specific number of digits of precision.
+  #
+  # = Parameters
+  # * +prec+ - The number of decimal digits of precision in the computed value.
+  #   Must be a positive integer (otherwise an ArgumentError is raised).
+  #
+  # = Example
+  #
+  #   require 'bigdecimal'
+  #   require 'bigdecimal/math'
+  #   include BigMath
+  #
+  #   puts TAU(100)
+  #
   def TAU(prec)
     raise ArgumentError, "Zero or negative argument for TAU" if prec <= 0
     PI(prec) * BigDecimal("2")
