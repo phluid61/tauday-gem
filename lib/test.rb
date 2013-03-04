75f4158b7b39715127e4be783b90521bd07cc3cc
Matthew Kerwin <matthew@kerwin.net.au>
2013-03-04 12:26:46 +1000

    added (unreliable\?) unit tests

---
+++ lib/test.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby --
+
+require 'test/unit'
+
+require 'bigdecimal'
+require 'bigdecimal/math'
+require 'tauday'
+
+class TestTauday < Test::Unit::TestCase
+	def test_tau
+		# 6.283185307179586..
+		assert_equal(Math::TAU, Math::PI*2.0)
+	end
+
+	include BigMath
+	def test_TAU
+		assert_equal(TAU(1).round(1).to_s('F'), '6.3')
+		assert_equal(TAU(100).truncate(100).to_s('F'), '6.2831853071795864769252867665590057683943387987502116419498891846156328125724179972560696506842341359')
+	end
+end
+
