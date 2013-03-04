f2fdd9a88095453f4b9df1d8159694757a91b097
Matthew Kerwin <matthew@kerwin.net.au>
2013-03-04 12:31:02 +1000

    added failure tests

--- lib/test.rb
+++ lib/test.rb
@@ -16,6 +16,9 @@ class TestTauday < Test::Unit::TestCase
 	def test_TAU
 		assert_equal(TAU(1).round(1).to_s('F'), '6.3')
 		assert_equal(TAU(100).truncate(100).to_s('F'), '6.2831853071795864769252867665590057683943387987502116419498891846156328125724179972560696506842341359')
+		assert_raise(ArgumentError) { TAU(0) }
+		assert_raise(ArgumentError) { TAU(-3) }
+		assert_raise(ArgumentError) { TAU('barf') }
 	end
 end
 
